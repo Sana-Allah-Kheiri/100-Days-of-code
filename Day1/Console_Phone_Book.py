16d2f35a806c30b4d94d8f4f37ee1efdd1d89f76
SanaAllah Kheiri <69618835+Sana-Allah-Kheiri@users.noreply.github.com>
2026-07-25 07:25:10 +0330

    Update Console_Phone_Book.py

--- Day1/Console_Phone_Book.py
+++ Day1/Console_Phone_Book.py
@@ -1,1 +1,6 @@
-
+# By Sasan Ace at https://github.com/Sana-Allah-Kheiri/100-Days-of-code
+"""
+According to the 14th video of Day1 , I thought it might be cool to write a script for phone book
+because when you store numbers in your contact list, 
+it looks like you assign a string (which is a phoneNumber here) to a variable called contact name
+"""
